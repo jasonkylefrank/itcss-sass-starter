51d0bc1d60031fe9cfb24e3d2cd25e1d917b2cec
Jason K. Frank <jasonkylefrank@gmail.com>
2015-10-27 13:11:34 -0400

    cleaned up node modules that apparently aren't being used anymore, then tried modifying some files to see them being injected

--- assets/main.js
+++ assets/main.js
@@ -7,14 +7,28 @@ import ReactDOM from 'react-dom';
 class Group extends React.Component {
     displayName: 'Group'
 
+    getFakeStuff(fakeCount) {
+        var fakeStuff = [];
+
+        for (var i = 0; i < fakeCount; i++) {
+          fakeStuff.push(<div>This is fake stuff</div>);
+        }
+
+        return fakeStuff;
+    }
+
     render() {
+        var stuff = this.getFakeStuff(7);
+
         return (
 
             <div className='container'>
                 <h1> Hello Again, Sup dfgdg</h1>
-      
+
                 <input placeholder='dsfdst'/>
                 <p>Test d ffAnother Onedfdf</p>
+
+                {stuff}
             </div>
 
         )
@@ -25,4 +39,3 @@ class Group extends React.Component {
 
 
 ReactDOM.render(<Group/>, document.getElementById('react'));
-
